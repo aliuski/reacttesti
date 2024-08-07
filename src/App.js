30ae9e14da5330ba08c4c9a8889271440e113c67
Aki Liuski <aki.liuski@tietoevry.com>
2024-08-07 13:03:32 +0300

    Muutoksia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import axios from "axios";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [inni, setInni] = React.useState('');
+  const [value, setValue] = React.useState('-');
+  function handleClick(){
+	axios.get('http://akispringboot.eu-north-1.elasticbeanstalk.com/greeting?name='+inni).then((response) => {
+        setValue(response.data.content);
+      });
+  }
+  return <div>
+  <input value={inni} onChange={e=>setInni(e.target.value)} />
+  <button onClick={handleClick}>Nappi</button><br/>{value}</div>;
 }
 
 export default App;
